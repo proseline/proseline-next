c49873e2899fcb7fa8109f7338bf4a6abff7d4ff
Kyle E. Mitchell <kyle@kemitchell.com>
2020-11-19 12:00:31 -0800

    Add comment

--- stringify.js
+++ stringify.js
@@ -1,1 +1,10 @@
+// This module exports a JSON stringify function.
+//
+// To make sure equivalent JSON data always stringifies to
+// the same, identical string, we use an implementation that
+// outputs the keys of every object in sorted order. This gives
+// us "content addressability", the ability to refer to a data
+// record by the cryptographic digest of its canonical string
+// representation.
+
 module.exports = require('fast-json-stable-stringify')
